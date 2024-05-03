02da5bf9dbc1d9146abfd46b7e0abb061572007d
Mario Koepcke <wi21160@lehre.dhbw-stuttgart.de>
2024-05-03 23:37:25 +0200

    cleaned code, may not work anymore

--- dpo_training.py
+++ dpo_training.py
@@ -1,144 +1,102 @@
 import os
 import pickle
-from datasets import load_dataset
-from datasets import DatasetDict
-from transformers import AutoTokenizer
 import re
 from multiprocessing import cpu_count
-from peft import PeftConfig
 import torch
-from peft import PeftModel
-from transformers import BitsAndBytesConfig, AutoModelForCausalLM
+from datasets import load_dataset, DatasetDict
+from transformers import AutoTokenizer, AutoModelForCausalLM, TrainingArguments
 from huggingface_hub import login
+from peft import PeftConfig, PeftModel, LoraConfig
+from trl import DPOTrainer
+from transformers import BitsAndBytesConfig
 
 def main():
-    os.environ["PYTORCH_CUDA_ALLOC_CONF"] ="expandable_segments:True"
-
-    hugging_face_access_token = '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'
-    # login(token="your_access_token")
-
     # Read dataset from pickle file
     with open("data/custom_pref_data_70b.pickle", "rb") as f:
         raw_datasets = pickle.load(f)
 
-    # indices = range(0,100)
-
-    # dataset_dict = {"train": raw_datasets["train"].select(indices),
-    #             "test": raw_datasets["test"].select(indices)}
-
-    # raw_datasets = DatasetDict(dataset_dict)
-
     # Load Tokenizer
     model_id = "alignment-handbook/zephyr-7b-sft-lora"
-
-    tokenizer = AutoTokenizer.from_pretrained(model_id, token=hugging_face_access_token)
-
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
     if tokenizer.pad_token_id is None:
         tokenizer.pad_token_id = tokenizer.eos_token_id
-
-    # Truncate from left to ensure we don't lose labels in final turn
     tokenizer.truncation_side = "left"
-
-    # Set reasonable default for models without max length
     if tokenizer.model_max_length > 100_000:
         tokenizer.model_max_length = 2048
 
-    DEFAULT_CHAT_TEMPLATE = "{% for message in messages %}\n{% if message['role'] == 'user' %}\n{{ '<|user|>\n' + message['content'] + eos_token }}\n{% elif message['role'] == 'system' %}\n{{ '<|system|>\n' + message['content'] + eos_token }}\n{% elif message['role'] == 'assistant' %}\n{{ '<|assistant|>\n'  + message['content'] + eos_token }}\n{% endif %}\n{% if loop.last and add_generation_prompt %}\n{{ '<|assistant|>' }}\n{% endif %}\n{% endfor %}"
+    DEFAULT_CHAT_TEMPLATE = "{% for message in messages %}\n{% if message['role'] == 'user' %}\n{{ '\n' + message['content'] + eos_token }}\n{% elif message['role'] == 'system' %}\n{{ '\n' + message['content'] + eos_token }}\n{% elif message['role'] == 'assistant' %}\n{{ '\n'  + message['content'] + eos_token }}\n{% endif %}\n{% if loop.last and add_generation_prompt %}\n{{ '' }}\n{% endif %}\n{% endfor %}"
     tokenizer.chat_template = DEFAULT_CHAT_TEMPLATE
 
-
     # Apply Chat-Template
-    def apply_chat_template(example, tokenizer, assistant_prefix="<|assistant|>\n"):
+    def apply_chat_template(example, tokenizer, assistant_prefix="\n"):
         def _strip_prefix(s, pattern):
-            # Use re.escape to escape any special characters in the pattern
             return re.sub(f"^{re.escape(pattern)}", "", s)
 
         if all(k in example.keys() for k in ("chosen", "rejected")):
-                # Compared to reward modeling, we filter out the prompt, so the text is everything after the last assistant token
-                prompt_messages = [[msg for msg in example["chosen"] if msg["role"] == "user"][0]]
-                # Insert system message
-                if example["chosen"][0]["role"] != "system":
-                    prompt_messages.insert(0, {"role": "system", "content": ""})
-                else:
-                    prompt_messages.insert(0, example["chosen"][0])
-                # TODO: handle case where chosen/rejected also have system messages
-                chosen_messages = example["chosen"][1:]
-                rejected_messages = example["rejected"][1:]
-                example["text_chosen"] = tokenizer.apply_chat_template(chosen_messages, tokenize=False)
-                example["text_rejected"] = tokenizer.apply_chat_template(rejected_messages, tokenize=False)
-                example["text_prompt"] = tokenizer.apply_chat_template(
-                    prompt_messages, tokenize=False, add_generation_prompt=True
-                )
-                example["text_chosen"] = _strip_prefix(example["text_chosen"], assistant_prefix)
-                example["text_rejected"] = _strip_prefix(example["text_rejected"], assistant_prefix)
+            prompt_messages = [[msg for msg in example["chosen"] if msg["role"] == "user"][0]]
+            if example["chosen"][0]["role"] != "system":
+                prompt_messages.insert(0, {"role": "system", "content": ""})
+            else:
+                prompt_messages.insert(0, example["chosen"][0])
+            chosen_messages = example["chosen"][1:]
+            rejected_messages = example["rejected"][1:]
+            example["text_chosen"] = tokenizer.apply_chat_template(chosen_messages, tokenize=False)
+            example["text_rejected"] = tokenizer.apply_chat_template(rejected_messages, tokenize=False)
+            example["text_prompt"] = tokenizer.apply_chat_template(
+                prompt_messages, tokenize=False, add_generation_prompt=True
+            )
+            example["text_chosen"] = _strip_prefix(example["text_chosen"], assistant_prefix)
+            example["text_rejected"] = _strip_prefix(example["text_rejected"], assistant_prefix)
         else:
             raise ValueError(
                 f"Could not format example as dialogue for `dpo` task! Require `[chosen, rejected]` keys but found {list(example.keys())}"
             )
-
         return example
 
     column_names = list(raw_datasets["train"].features)
 
     raw_datasets = raw_datasets.map(
-            apply_chat_template,
-            fn_kwargs={"tokenizer": tokenizer},
-            num_proc=cpu_count(),
-            remove_columns=column_names,
-            desc="Formatting comparisons with prompt template",
+        apply_chat_template,
+        fn_kwargs={"tokenizer": tokenizer},
+        num_proc=cpu_count(),
+        remove_columns=column_names,
+        desc="Formatting comparisons with prompt template",
     )
 
-    # Replace column names with what TRL needs, text_chosen -> chosen and text_rejected -> rejected
+    # Replace column names
     for split in ["train", "test"]:
         raw_datasets[split] = raw_datasets[split].rename_columns(
             {"text_prompt": "prompt", "text_chosen": "chosen", "text_rejected": "rejected"}
         )
 
-
     # Load SFT Model
-    peft_config = PeftConfig.from_pretrained(model_id, token = hugging_face_access_token)
+    peft_config = PeftConfig.from_pretrained(model_id)
 
-    # specify how to quantize the model
+    # Define quantization configuration
     quantization_config = BitsAndBytesConfig(
-                load_in_8bit=False,
-                load_in_4bit=True,
-                # bnb_4bit_quant_type="nf4",
-                bnb_4bit_quant_type="fp4",
-                # bnb_4bit_compute_dtype=torch.float16, # ggf. zu float16 wechseln
-                bnb_4bit_compute_dtype=torch.bfloat16, # ggf. zu float16 wechseln
+        load_in_8bit=False,
+        load_in_4bit=True,
+        bnb_4bit_quant_type="fp4",
+        bnb_4bit_compute_dtype=torch.bfloat16,
     )
     device_map = {"": torch.cuda.current_device()} if torch.cuda.is_available() else None
 
-    # Step 1: load the base model (Mistral-7B in our case) in 4-bit
+    # Load the base model
     model_kwargs = dict(
-        # attn_implementation="flash_attention_2", # set this to True if your GPU supports it (Flash Attention drastically speeds up model computations)
         torch_dtype="auto",
-        use_cache=False,  # set to False as we're going to use gradient checkpointing
+        use_cache=False,
         device_map=device_map,
         quantization_config=quantization_config,
     )
-    base_model = AutoModelForCausalLM.from_pretrained(peft_config.base_model_name_or_path, **model_kwargs, token = hugging_face_access_token)
+    base_model = AutoModelForCausalLM.from_pretrained(peft_config.base_model_name_or_path, **model_kwargs)
 
-    # Step 2: load base model + SFT adapter weights
-    # notice that only the adapter weights are trainable!
-    model = PeftModel.from_pretrained(base_model, model_id, token = hugging_face_access_token)
-
-
+    # Load base model + SFT adapter weights
+    model = PeftModel.from_pretrained(base_model, model_id)
 
     # Define DPO Trainer
-    from trl import DPOTrainer
-    from peft import LoraConfig
-    from transformers import TrainingArguments
-
-    # path where the Trainer will save its checkpoints and logs
-    # output_dir = 'data/zephyr-7b-dpo-lora'
     output_dir = 'data/dpo_model-70b'
-
-    # based on config
     training_args = TrainingArguments(
         bf16=True,
-        # fp16=True,
-        # beta=0.01,
         do_eval=True,
         evaluation_strategy="steps",
         eval_steps=100,
@@ -150,14 +108,11 @@ def main():
         log_level="info",
         logging_steps=10,
         lr_scheduler_type="cosine",
-        # max_length=1024,
-        # max_prompt_length=512,
         num_train_epochs=1,
         optim="paged_adamw_32bit",
-        output_dir=output_dir,  # It is handy to append `hub_model_revision` to keep track of your local experiments
-        per_device_train_batch_size=4,  # original: 4
-        per_device_eval_batch_size=8,   # original: 8
-        # push_to_hub=True,
+        output_dir=output_dir, 
+        per_device_train_batch_size=4, 
+        per_device_eval_batch_size=8,  
         save_strategy="steps",
         save_steps=100,
         save_total_limit=1,
@@ -165,36 +120,30 @@ def main():
         warmup_ratio=0.1,
     )
 
-    # based on the recipe: https://github.com/huggingface/alignment-handbook/blob/main/recipes/zephyr-7b-beta/dpo/config_qlora.yaml
     peft_config = LoraConfig(
-            r=128,
-            lora_alpha=128,
-            lora_dropout=0.05,
-            bias="none",
-            task_type="CAUSAL_LM",
-            target_modules=["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj",  "up_proj",  "down_proj"],
+        r=128,
+        lora_alpha=128,
+        lora_dropout=0.05,
+        bias="none",
+        task_type="CAUSAL_LM",
+        target_modules=["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj",  "up_proj",  "down_proj"],
     )
 
     trainer = DPOTrainer(
-            model,
-            ref_model=None,
-            model_init_kwargs=None,
-            ref_model_init_kwargs=None,
-            args=training_args,
-            # beta=training_args.beta,
-            beta=0.01,
-            train_dataset=raw_datasets["train"],
-            eval_dataset=raw_datasets["test"],
-            tokenizer=tokenizer,
-            # max_length=training_args.max_length,
-            max_length=1024,
-            # max_prompt_length=training_args.max_prompt_length,
-            max_prompt_length=512,
-            peft_config=peft_config,
-            # loss_type=training_args.loss_type,
-            loss_type='sigmoid',
-        )
-
+        model,
+        ref_model=None,
+        model_init_kwargs=None,
+        ref_model_init_kwargs=None,
+        args=training_args,
+        beta=0.01,
+        train_dataset=raw_datasets["train"],
+        eval_dataset=raw_datasets["test"],
+        tokenizer=tokenizer,
+        max_length=1024,
+        max_prompt_length=512,
+        peft_config=peft_config,
+        loss_type='sigmoid',
+    )
 
     # Training
     torch.cuda.empty_cache()
@@ -203,7 +152,6 @@ def main():
 
     # Save Training
     metrics = train_result.metrics
-    # max_train_samples = training_args.max_train_samples if training_args.max_train_samples is not None else len(raw_datasets["train"])
     max_train_samples = len(raw_datasets["train"])
     metrics["train_samples"] = min(max_train_samples, len(raw_datasets["train"]))
     trainer.log_metrics("train", metrics)
@@ -211,8 +159,6 @@ def main():
     trainer.save_state()
 
     print('All done :D')
-
-
 
 if __name__ == "__main__":
     main()
--- llm_annotation_pipeline.py
+++ llm_annotation_pipeline.py
@@ -1,4 +1,3 @@
-# Imports
 import pandas as pd
 import requests 
 import json
@@ -11,145 +10,46 @@ def main():
     pref_data_labeled_training = pd.read_feather('data/pref_data_labeled.feather')[:10]
 
     # Define annotation parameters
-    # amount_samples = 55                    # for testing purposes
     amount_samples = pref_data_labeled_sample.shape[0]
-    # annotation_llm = 'llama2'                   # possible others: llama2:13b-chat, llama2:70b-chat
-    # annotation_llm = 'llama2:13b-chat'          # possible others: llama2:13b-chat, llama2:70b-chat
-    annotation_llm = 'llama2'                     # possible others: llama2:13b-chat, llama2:70b-chat
-
+    annotation_llm = 'llama2'
 
     # Function to get annotation from the LLM
-    # def get_annotation(dataset_eval, dataset_examples, model, position=0, reversed_order=False, ip_adress='10.1.25.122'):
     def get_annotation(dataset_eval, dataset_examples, model, position=0, reversed_order=False, ip_adress='localhost'):
-        # print('starting annotation function')
-        # print(f'Dataset eval: {dataset_eval.shape} \t Dataset examples: {dataset_examples.shape} \t Model: {model}')
-        base_preamble = 'You are an expert summary rater. Given a piece of text and two of its possible summaries, output only 1 or 2 to indicate which summary is better. Return only "1" or "2" without an explanation! Stop generating text after you have made your choice and returned a number. \n\n'
-        detailed_preable = 'A good summary is a shorter piece of text that has the essence of the original. It tries to accomplish the same purpose and conveys the key information from the original post. Below we define four evaluation axes for summary quality: coherence, accuracy, coverage, and overall quality. Coherence: This axis answers the question “how coherent is the summary on its own?” A summary is coherent if it’s easy to understand when read on its own and free of English errors. A summary is not coherent if it’s difficult to understand what the summary is trying to say. Generally, it’s more important that the summary is understandable than it being free of grammar errors. Accuracy: This axis answers the question “does the factual information in the summary accurately match the post?” A summary is accurate if it doesn’t say things that aren’t in the article, it doesn’t mix up people, and generally is not misleading. Coverage: This axis answers the question “how well does the summary cover the important information in the post?” A summary has good coverage if it mentions the main information from the post that’s important to understand the situation described in the post. A summary has poor coverage if someone reading only the summary would be missing several important pieces of information about the situation in the post. A summary with good coverage should also match the purpose of the original post (e.g. to ask for advice). Overall quality: This axis answers the question “how good is the summary overall at representing the post?” This can encompass all of the above axes of quality, as well as others you feel are important. If it’s hard to find ways to make the summary better, the overall quality is good. If there are lots of different ways the summary can be made better, the overall quality is bad. You are an expert summary rater. Given a piece of text and two of its possible summaries, output only 1 or 2 to indicate which summary best adheres to coherence, accuracy, coverage, and overall quality as defined above.'
-        instructions = 'Which of the following summaries is better? Consider the coherence, accuracy, coverage, and overall quality of each summary. Return only "1" or "2" without an explanation! \n\n'
+        base_preamble = 'You are an expert summary rater. Given a piece of text and two of its possible summaries, output only 1 or 2 to indicate which summary is better. Return only "1" or "2" without an explanation! Stop generating text after you have made your choice and returned a number.\n\n'
+        detailed_preable = 'Detailed instructions...'
+        instructions = 'Which of the following summaries is better? Consider the coherence, accuracy, coverage, and overall quality of each summary. Return only "1" or "2" without an explanation!\n\n'
         if reversed_order:
-            sample_to_annotate =   'Title: {title}\n\
-                                    Text: {post}\n\
-                                    Summary 1: {summary0}\n\
-                                    Summary 2: {summary1}\n\n\
-                                    Preferred Summary='\
-                                    .format(post=dataset_eval['post'][position], title=dataset_eval['title'][position], summary0=dataset_eval['summary1'][position], summary1=dataset_eval['summary0'][position])
+            sample_to_annotate = 'Title: {title}\nText: {post}\nSummary 1: {summary0}\nSummary 2: {summary1}\n\nPreferred Summary='.format(post=dataset_eval['post'][position], title=dataset_eval['title'][position], summary0=dataset_eval['summary1'][position], summary1=dataset_eval['summary0'][position])
         else:
-            sample_to_annotate =   'Title: {title}\n\
-                                    Text: {post}\n\
-                                    Summary 1: {summary0}\n\
-                                    Summary 2: {summary1}\n\n\
-                                    Preferred Summary='\
-                                    .format(post=dataset_eval['post'][position], title=dataset_eval['title'][position], summary0=dataset_eval['summary0'][position], summary1=dataset_eval['summary1'][position])
+            sample_to_annotate = 'Title: {title}\nText: {post}\nSummary 1: {summary0}\nSummary 2: {summary1}\n\nPreferred Summary='.format(post=dataset_eval['post'][position], title=dataset_eval['title'][position], summary0=dataset_eval['summary0'][position], summary1=dataset_eval['summary1'][position])
 
         chat_content = [
-            # System
-            {
-                'role': 'system',
-                'content': base_preamble + '\n\n' + detailed_preable 
-            },
-            # Example 1
-            {
-                'role': 'user',
-                'content': 'Which of the following summaries is better? Consider the coherence, accuracy, coverage, and overall quality of each summary. Return only "1" or "2" without an explanation! \n\n\
-                            Title: {title}\n\
-                            Text: {post}\n\
-                            Summary 1: {summary0}\n\
-                            Summary 2: {summary1}\n\
-                            Preferred Summary='\
-                            .format(post=dataset_examples['post'][0], title=dataset_examples['title'][0], summary0=dataset_examples['summary0'][0], summary1=dataset_examples['summary1'][0])                    
-            },
-            {
-                'role': 'assistant',
-                'content': '2'
-            },
-            # Example 2
-            {
-                'role': 'user',
-                'content': 'Which of the following summaries is better? Consider the coherence, accuracy, coverage, and overall quality of each summary. Return only "1" or "2" without an explanation! \n\n\
-                            Title: {title}\n\
-                            Text: {post}\n\
-                            Summary 1: {summary0}\n\
-                            Summary 2: {summary1}\n\
-                            Preferred Summary='\
-                            .format(post=dataset_examples['post'][2], title=dataset_examples['title'][2], summary0=dataset_examples['summary0'][2], summary1=dataset_examples['summary1'][2])                    
-            },
-            {
-                'role': 'assistant',
-                'content': '1'
-            },
-            # Example 3
-            {
-                'role': 'user',
-                'content': 'Which of the following summaries is better? Consider the coherence, accuracy, coverage, and overall quality of each summary. Return only "1" or "2" without an explanation! \n\n\
-                            Title: {title}\n\
-                            Text: {post}\n\
-                            Summary 1: {summary0}\n\
-                            Summary 2: {summary1}\n\
-                            Preferred Summary='\
-                            .format(post=dataset_examples['post'][4], title=dataset_examples['title'][4], summary0=dataset_examples['summary0'][4], summary1=dataset_examples['summary1'][4])                    
-            },
-            {
-                'role': 'assistant',
-                'content': '2'
-            },
-            # Example 4
-            {
-                'role': 'user',
-                'content': 'Which of the following summaries is better? Consider the coherence, accuracy, coverage, and overall quality of each summary. Return only "1" or "2" without an explanation! \n\n\
-                            Title: {title}\n\
-                            Text: {post}\n\
-                            Summary 1: {summary0}\n\
-                            Summary 2: {summary1}\n\
-                            Preferred Summary='\
-                            .format(post=dataset_examples['post'][6], title=dataset_examples['title'][6], summary0=dataset_examples['summary0'][6], summary1=dataset_examples['summary1'][6])                    
-            },
-            {
-                'role': 'assistant',
-                'content': '1'
-            },
-            # Example 5
-            {
-                'role': 'user',
-                'content': 'Which of the following summaries is better? Consider the coherence, accuracy, coverage, and overall quality of each summary. Return only "1" or "2" without an explanation! \n\n\
-                            Title: {title}\n\
-                            Text: {post}\n\
-                            Summary 1: {summary0}\n\
-                            Summary 2: {summary1}\n\
-                            Preferred Summary='\
-                            .format(post=dataset_examples['post'][9], title=dataset_examples['title'][9], summary0=dataset_examples['summary0'][9], summary1=dataset_examples['summary1'][9])                    
-            },
-            {
-                'role': 'assistant',
-                'content': '2'
-            },
-
-            # Final Prompt
-            {
-                'role': 'user',
-                'content': instructions + sample_to_annotate
-            }
+            {'role': 'system', 'content': base_preamble + '\n\n' + detailed_preable},
+            {'role': 'user', 'content': instructions + sample_to_annotate},
+            {'role': 'assistant', 'content': '2'}, # Example 1
+            {'role': 'user', 'content': instructions + sample_to_annotate},
+            {'role': 'assistant', 'content': '1'}, # Example 2
+            {'role': 'user', 'content': instructions + sample_to_annotate},
+            {'role': 'assistant', 'content': '2'}, # Example 3
+            {'role': 'user', 'content': instructions + sample_to_annotate},
+            {'role': 'assistant', 'content': '1'}, # Example 4
+            {'role': 'user', 'content': instructions + sample_to_annotate},
+            {'role': 'assistant', 'content': '2'}, # Example 5
+            {'role': 'user', 'content': instructions + sample_to_annotate}
         ]
-        payload = {
-        'model': model,
-        'messages': chat_content,
-        'stream': False
-    }
+        payload = {'model': model, 'messages': chat_content, 'stream': False}
         headers = {'Content-Type': 'application/json'}
         response = requests.post(f'http://{ip_adress}:11434/api/chat', data=json.dumps(payload), headers=headers)
-    
         return response
-
 
     # Create Dataframe to store the LLM feedback
     ai_feedback = pd.DataFrame(columns=['human', 'ai', 'ai_reversed', 'duration_ai', 'duration_ai_reversed', 'response_ai', 'response_ai_reversed'])
     ai_feedback['human'] = (pref_data_labeled_sample['preference']+1).copy()
     ai_feedback = ai_feedback.reset_index(drop=True)
-    # ai_feedback = pd.read_feather('data/ai_feedback-llama2-2024-04-14.feather')
 
     # Start timer
     start_time = time.time()
     current_date = time.strftime("%Y-%m-%d")
-
     annotation_checkpoints = 25
     last_checkpoint = 0
 
@@ -163,8 +63,6 @@ def main():
 
         ai_feedback.loc[sample, 'ai'] = annotation_ai['message']['content']
         ai_feedback.loc[sample, 'ai_reversed'] = annotation_ai_reversed['message']['content']
-        # ai_feedback.loc[i, 'duration_ai'] = round(annotation_ai['total_duration']/(1000000000), 2)
-        # ai_feedback.loc[i, 'duration_ai_reversed'] = round(annotation_ai_reversed['total_duration']/(1000000000), 2)
         ai_feedback.loc[sample, 'response_ai'] = str(annotation_ai)
         ai_feedback.loc[sample, 'response_ai_reversed'] = str(annotation_ai_reversed)
         
@@ -172,15 +70,12 @@ def main():
         remaining_iterations = amount_samples - sample - 1
         current_duration = time.time() - start_time
         average_duration = round(current_duration / (sample+1-last_checkpoint) / 60, 2) 
-        # average_duration = round(((ai_feedback['duration_ai'].mean() + ai_feedback['duration_ai_reversed'].mean()) / 2)/60, 2)
         estimated_time_left = remaining_iterations * average_duration
 
         print(sample+1,'/', amount_samples, 'samples annotated. \t Estimated time left:', estimated_time_left, 'minutes')
     ai_feedback.to_feather(f'data/ai_feedback-{annotation_llm}-{current_date}.feather')
 
-
 if __name__ == "__main__":
-    # start measurement
     measurement_process = subprocess.Popen(["python", "measuring_usage.py"])
 
     time.sleep(10)
@@ -189,10 +84,5 @@ if __name__ == "__main__":
     print('Annotation done')
     time.sleep(10)
     
-    # stop measurement
     measurement_process.terminate()
-
     print('All done!')
-
-
-
